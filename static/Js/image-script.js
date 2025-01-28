499b172fe7487f29a7997a8838a01a0572497997
NikiUgarin <159936521+NikiUgarin@users.noreply.github.com>
2025-01-28 17:04:19 +0300

    Add files via upload

--- static/Js/image-script.js
+++ static/Js/image-script.js
@@ -2,11 +2,24 @@ const myphoto = document.getElementById('myphoto');
 let currentImage = 'static/img/myphoto2_anonymized.png';
 
 myphoto.addEventListener('click', () => {
-    if (currentImage === 'static/img/myphoto2_anonymized.png') {
-        myphoto.src = 'static/img/myphoto2.jpg';
-        currentImage = 'static/img/myphoto2.jpg';
-    } else {
-        myphoto.src = 'static/img/myphoto2_anonymized.png';
-        currentImage = 'static/img/myphoto2_anonymized.png';
-    }
+    // Добавляем класс "rotating" для запуска анимации
+    myphoto.classList.add('rotating');
+
+    // Функция для изменения изображения после завершения анимации
+    const onAnimationEnd = () => {
+        if (currentImage === 'static/img/myphoto2_anonymized.png') {
+            myphoto.src = 'static/img/myphoto2.jpg';
+            currentImage = 'static/img/myphoto2.jpg';
+        } else {
+            myphoto.src = 'static/img/myphoto2_anonymized.png';
+            currentImage = 'static/img/myphoto2_anonymized.png';
+        }
+
+        // Удаляем класс "rotating" после завершения анимации
+        myphoto.classList.remove('rotating');
+        myphoto.removeEventListener('animationend', onAnimationEnd);
+    };
+
+    // Добавляем обработчик события для завершения анимации
+    myphoto.addEventListener('animationend', onAnimationEnd, { once: true });
 });
